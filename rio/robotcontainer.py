09b4de07b0eb11d65d84b34eb69065ec2193b6e5
kredcool <kredcool2007@outlook.com>
2024-01-13 12:42:12 -0500

    this run's

--- rio/robotcontainer.py
+++ rio/robotcontainer.py
@@ -79,51 +79,5 @@ class RobotContainer:
         """
 
     def getAutonomousCommand(self) -> commands2.Command:
-        # Create config for trajectory
-        config = TrajectoryConfig(
-            self.autoConst["maxSpeedMetersPerSecond"],
-            self.autoConst["maxAccelerationMetersPerSecondSquared"],
-        )
-        # ensure max speed is obeyed
-        config.setKinematics(DriveConstants.driveKinematics)
-
-        # An example trajectory to follow. All units in meters.
-        exampleTrajectory = TrajectoryGenerator.generateTrajectory(
-            # Start at the origin facing the +X direction
-            Pose2d(0, 0, Rotation2d(0)),
-            # Pass through these two interior waypoints, making an 's' curve path
-            [Translation2d(1, 1), Translation2d(2, -1)],
-            # End 3 meters straight ahead of where we started, facing forward
-            Pose2d(3, 0, Rotation2d(0)),
-            config,
-        )
-
-        thetaController = ProfiledPIDControllerRadians(
-            self.autoConst["pThetaController"],
-            0,
-            0,
-            AutoConstants.thetaControllerConstraints,
-        )
-        thetaController.enableContinuousInput(-math.pi, math.pi)
-
-        swerveControllerCommand = SwerveControllerCommand(
-            exampleTrajectory,
-            self.robotDrive.getPose(),
-            DriveConstants.driveKinematics,
-            PIDController(self.autoConst["pXController"], 0, 0),
-            PIDController(self.autoConst["pYController"], 0, 0),
-            thetaController,
-            self.robotDrive.setModuleStates(),
-            (self.robotDrive,),
-        )
-
-        # Reset odometry to starting pose
-        self.robotDrive.resetOdometry(exampleTrajectory.initialPose())
-
-        # Run path following command, then stop at the end
-        return swerveControllerCommand.andThen(
-            cmd.run(
-                lambda: self.robotDrive.drive(0, 0, 0, False, False),
-                self.robotDrive,
-            )
-        )
+        # rev has stuff here but I don't want to debug it
+        pass
