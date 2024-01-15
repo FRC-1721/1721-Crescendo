b918e3507f8a7f0ea161d00bdf62ecc860572911
dublUayaychtee <mail@dublu.net>
2024-01-15 12:31:33 -0500

    better error message

--- dashboard/src/canvas.js
+++ dashboard/src/canvas.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("cool");
 const gl = canvas.getContext("webgl");
 if (!gl) {
-    alert("Your browser does not support WebGL.");
+    alert("yro;ue browser not habbing webgl >:(");
 }
 gl.clearColor(0, 1, 0, 1);
 gl.clear(gl.COLOR_BUFFER_BIT);
