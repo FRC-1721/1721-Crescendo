35de0cf01a700333a19e3749f2614def737bca3f
kredcool <kredcool2007@outlook.com>
2024-02-08 16:19:34 -0500

    waitcommand probalbly won't be needed upon review due to single thread

--- rio/commands/climb.py
+++ rio/commands/climb.py
@@ -18,10 +18,8 @@ class Climb(commands2.Command):
 
     def initialize(self):
         self.climberSubsystem.setServoAngle(ClimberConstants.kservoOpen)
-        commands2.waitcommand(1)  # to not grind on the servo lock
         self.climberSubsystem.setClimberMotorSpeed(self.speed)
 
     def end(self, interrupted: bool):
         self.climberSubsystem.setClimberMotorSpeed(0)
-        commands2.waitcommand(1)
         self.climberSubsystem.setServoAngle(ClimberConstants.kservolock)
