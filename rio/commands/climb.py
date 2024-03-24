12bdc8238b3e137964b89853fc9b0020b5b1e4fa
NotSimon5673 <JackJewett818@gmail.com>
2024-03-23 21:26:20 -0400

    lkgchgcvhbk

--- rio/commands/climb.py
+++ rio/commands/climb.py
@@ -1,10 +1,11 @@
 import commands2
 import rev
 
-from constants import ClimberConstants
+from constants import ClimberConstants, SuperStrucConstants
 
 from subsystems.climber import Climber
 from subsystems.shooter import Shooter
+import logging
 
 
 class Climb(commands2.Command):
@@ -34,7 +35,13 @@ class Climb(commands2.Command):
         self.climber.setServoAngle(ClimberConstants.kservoOpen)
 
     def execute(self):
-        self.climber.setClimberMotorSpeed(self.speed)
+        angle = self.shooter.getAngle()
+        softLimit = SuperStrucConstants.ShootPos - 20
+
+        if angle <= softLimit:
+            self.climber.setClimberMotorSpeed(self.speed)
+        else:
+            logging.warn(f"You've gone too far! {angle}/{softLimit}")
         self.shooter.setIdleMode(rev._rev.CANSparkBase.IdleMode.kBrake)
 
     def end(self, interrupted: bool):
